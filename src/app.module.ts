dbcf39e756f2b7e7e106867321db041d3ee59aeb
chlee1 <chlee1@ntels.com>
2022-02-14 16:54:09 +0900

    Add MongoExceptionFileter to send the response with conflict message when mongoInsert failed caused by conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SchoolsModule } from './schools/schools.module';
 import { UsersModule } from './users/users.module';
 import { NewsModule } from './news/news.module';
+import { MongoExceptionFilter } from './util/mongoeException.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,6 +16,12 @@ import { NewsModule } from './news/news.module';
     NewsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: MongoExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
